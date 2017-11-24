f344eb52299b70b34297f0f81d766339e74a016a
Ricardo Cino <ricardo@cino.io>
2017-11-24 10:48:11 +0100

    Add auto download/update url's

--- autofill-checkout.js
+++ autofill-checkout.js
@@ -10,6 +10,8 @@
 // @match        *.demo.trvl.yt/book?page=checkout*
 // @match        *.trvl.com/book?page=checkout*
 // @require      http://code.jquery.com/jquery-3.2.1.min.js
+// @updateURL    https://raw.githubusercontent.com/Trvl/tampermonkey/master/autofill-checkout.js
+// @downloadURL  https://raw.githubusercontent.com/Trvl/tampermonkey/master/autofill-checkout.js
 // @grant        none
 // ==/UserScript==
 
