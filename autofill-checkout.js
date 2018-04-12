4bf791ca635ecba4b3b3779a001ab1a7eee5d61b
Osinga <victor@trvl.com>
2018-04-12 16:18:27 +0200

    Use vanilla JavaScript

--- autofill-checkout.js
+++ autofill-checkout.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TRVL Checkout Autofill
 // @namespace    https://trvl.com/
-// @version      0.1.8
+// @version      0.2.0
 // @description  Populate all input fields on the checkout pages
 // @homepageURL  https://github.com/TRVL/tampermonkey
 // @author       Ricardo Cino
@@ -10,7 +10,6 @@
 // @match        *.trvl-reboot.test/book*
 // @match        *.trvl.yt/book*
 // @match        *.trvl.com/book*
-// @require      http://code.jquery.com/jquery-3.2.1.min.js
 // @updateURL    https://rawgit.com/Trvl/tampermonkey/master/autofill-checkout.js
 // @downloadURL  https://rawgit.com/Trvl/tampermonkey/master/autofill-checkout.js
 // @supportURL   https://github.com/TRVL/tampermonkey/issues
@@ -18,31 +17,53 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
+    const change = new Event('change');
 
-    $('.checkout-button').before('<button type="button" id="fillform_tampermonkey" class="button checkout-button" style="background-color: #fff; border-color: #f19b39; color: #f19b39; float: right; margin-right: 24px;">Fill Form</button>');
+    const set = (id, value) => document.getElementById(id).value = value;
+    const update = id => document.getElementById(id).dispatchEvent(change);
 
-    $('#fillform_tampermonkey').click(function() {
-        $('#firstname').val('John');
-        $('#lastname').val('Batman');
-        $('#email').val(window.Laravel.user.email || '');
-        $('#phonenumber').val('612345678');
-        $('#phoneNumber').val('+31612345678');
-        $('#comments').val('Default request');
+    document.querySelector('.checkout-button').insertAdjacentHTML('afterend', `
+        <button
+            id="checkout-fill"
+            class="button checkout-button"
+            style="
+                background-color: #fff;
+                border-color: #f19b39;
+                color: #f19b39;
+                margin-right: 24px;
+            "
+            type="button"
+        >
+            Fill Form
+        </button>
+    `);
 
-        $('#creditCardFirstname').val('John');
-        $('#creditCardLastname').val('Batman');
-        $('#creditCardAddress').val('Herengracht 338');
-        $('#creditCardCity').val('Amsterdam');
-        $('#creditCardState').val('Noord Holland');
-        $('#creditCardZipcode').val('1016CG');
-        $("#creditCardCountry").val("NL").change();
+    document.getElementById('checkout-fill').addEventListener('click', () => {
+        if (window.route().current('bookings.create')) {
+            set('firstname', 'John');
+            set('lastname', 'Batman');
+            set('email', window.Laravel.user.email || '');
+            set('phonenumber', '612345678');
+            set('phoneNumber', '+31612345678');
+            set('comments', 'Default request');
+        } else if (window.route().current('bookings.payment')) {
+            set('creditCardAddress', 'Herengracht 338');
+            set('creditCardCity', 'Amsterdam');
+            set('creditCardState', 'Noord-Holland');
+            set('creditCardZipcode', '1016 CG');
+            set('creditCardCountry', 'NL');
 
-        // Fake creditcard
-        $("#creditCardType").val("CA").change();
-        $("#creditCardNumber").val("5275687365121871");
-        $("#creditCardExpirationMonth").val("2").change();
-        $("#creditCardExpirationYear").val("2019").change();
-        $("#creditCardIdentifier").val("845");
+            update('creditCardCountry');
+
+            set('creditCardFirstname', 'John');
+            set('creditCardLastname', 'Batman');
+            set('creditCardNumber', '5275687365121871');
+            set('creditCardExpirationMonth', '2');
+            set('creditCardExpirationYear', '2019');
+            set('creditCardIdentifier', '845');
+
+            update('creditCardExpirationMonth');
+            update('creditCardExpirationYear');
+        }
     });
 })();
