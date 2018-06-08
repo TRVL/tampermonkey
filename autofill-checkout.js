b3e8f85f781be2f5aae34a5dda86adf897bc7446
osinga <victor@trvl.com>
2018-06-08 10:58:40 +0200

    Clean up the mess

--- autofill-checkout.js
+++ autofill-checkout.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TRVL Checkout Autofill
 // @namespace    https://trvl.com/
-// @version      0.2.3
+// @version      0.2.4
 // @description  Populate all input fields on the checkout pages
 // @homepageURL  https://github.com/TRVL/tampermonkey
 // @author       Ricardo Cino
@@ -18,12 +18,20 @@
 
 (function() {
     const button = document.querySelector('.checkout-button');
-    const change = new Event('change');
 
     if (!button) return;
 
-    const set = (id, value) => document.getElementById(id).value = value;
-    const update = id => document.getElementById(id).dispatchEvent(change);
+    const blurEvent = new Event('blur');
+    const changeEvent = new Event('change');
+    const inputEvent = new Event('input');
+
+    const set = (id, value) => {
+        const el = document.getElementById(id);
+        el.value = value;
+        el.dispatchEvent(inputEvent);
+        el.dispatchEvent(blurEvent);
+    };
+    const update = id => document.getElementById(id).dispatchEvent(changeEvent);
 
     button.insertAdjacentHTML('afterend', `
         <button
@@ -43,42 +51,26 @@
     document.getElementById('checkout-fill').addEventListener('click', () => {
         if (window.route().current('bookings.create')) {
             set('firstName', 'John');
-            document.getElementById('firstName').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('lastName', 'Batman');
-            document.getElementById('lastName').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('email', window.Laravel.user.email || '');
-            document.getElementById('email').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('phonenumber', '612345678');
-            document.getElementById('phonenumber').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('phoneNumber', '+31612345678');
-            document.getElementById('phoneNumber').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('comments', 'Default request');
         } else if (window.route().current('bookings.payment')) {
             set('creditCardAddress', 'Herengracht 338');
-            document.getElementById('creditCardAddress').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('creditCardCity', 'Amsterdam');
-            document.getElementById('creditCardCity').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('creditCardState', 'Noord-Holland');
-            document.getElementById('creditCardState').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('creditCardZipCode', '1016 CG');
-            document.getElementById('creditCardZipCode').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('creditCardCountry', 'NL');
-            document.getElementById('creditCardCountry').dispatchEvent(new Event('input', { 'bubbles': true }));
 
             update('creditCardCountry');
 
             set('creditCardFirstName', 'John');
-            document.getElementById('creditCardFirstName').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('creditCardLastName', 'Batman');
-            document.getElementById('creditCardLastName').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('creditCardNumber', '5275687365121871');
-            document.getElementById('creditCardNumber').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('creditCardExpirationMonth', '2');
-            document.getElementById('creditCardExpirationMonth').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('creditCardExpirationYear', '2019');
-            document.getElementById('creditCardExpirationYear').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('creditCardIdentifier', '845');
-            document.getElementById('creditCardIdentifier').dispatchEvent(new Event('input', { 'bubbles': true }));
 
             update('creditCardExpirationMonth');
             update('creditCardExpirationYear');
