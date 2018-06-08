3d4b1d28b88b0b34005ddab001d57055a0a726b4
Arno Poot <arno@trvl.com>
2018-06-08 10:02:06 +0200

    Ugly fix for the new checkout

--- autofill-checkout.js
+++ autofill-checkout.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         TRVL Checkout Autofill
+// @name         TRVL Checkout AutofillNew
 // @namespace    https://trvl.com/
 // @version      0.2.3
 // @description  Populate all input fields on the checkout pages
@@ -43,26 +43,42 @@
     document.getElementById('checkout-fill').addEventListener('click', () => {
         if (window.route().current('bookings.create')) {
             set('firstName', 'John');
+            document.getElementById('firstName').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('lastName', 'Batman');
+            document.getElementById('lastName').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('email', window.Laravel.user.email || '');
+            document.getElementById('email').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('phonenumber', '612345678');
+            document.getElementById('phonenumber').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('phoneNumber', '+31612345678');
+            document.getElementById('phoneNumber').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('comments', 'Default request');
         } else if (window.route().current('bookings.payment')) {
             set('creditCardAddress', 'Herengracht 338');
+            document.getElementById('creditCardAddress').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('creditCardCity', 'Amsterdam');
+            document.getElementById('creditCardCity').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('creditCardState', 'Noord-Holland');
+            document.getElementById('creditCardState').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('creditCardZipCode', '1016 CG');
+            document.getElementById('creditCardZipCode').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('creditCardCountry', 'NL');
+            document.getElementById('creditCardCountry').dispatchEvent(new Event('input', { 'bubbles': true }));
 
             update('creditCardCountry');
 
             set('creditCardFirstName', 'John');
+            document.getElementById('creditCardFirstName').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('creditCardLastName', 'Batman');
+            document.getElementById('creditCardLastName').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('creditCardNumber', '5275687365121871');
+            document.getElementById('creditCardNumber').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('creditCardExpirationMonth', '2');
+            document.getElementById('creditCardExpirationMonth').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('creditCardExpirationYear', '2019');
+            document.getElementById('creditCardExpirationYear').dispatchEvent(new Event('input', { 'bubbles': true }));
             set('creditCardIdentifier', '845');
+            document.getElementById('creditCardIdentifier').dispatchEvent(new Event('input', { 'bubbles': true }));
 
             update('creditCardExpirationMonth');
             update('creditCardExpirationYear');
